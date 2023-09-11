4e34199f752f9ddf4625a6b734b0a63efc76107f
Tony Bo <tony.bo@databricks.com>
2023-09-11 11:38:49 -0400

    keyvault comments

--- azure/tf/modules/azure_hub/keyvault.tf
+++ azure/tf/modules/azure_hub/keyvault.tf
@@ -1,4 +1,4 @@
-# Why do `key_opts` and `key_permissions` differ in terms of required capitalization?
+# Define the Azure Key Vault resource
 resource "azurerm_key_vault" "this" {
   name                = "example-hub-keyvault"
   location            = azurerm_resource_group.hub.location
@@ -10,6 +10,7 @@ resource "azurerm_key_vault" "this" {
   soft_delete_retention_days = 7
 }
 
+# Define a key in the Azure Key Vault for managed services
 resource "azurerm_key_vault_key" "managed_services" {
   depends_on = [azurerm_key_vault_access_policy.terraform]
 
@@ -18,6 +19,7 @@ resource "azurerm_key_vault_key" "managed_services" {
   key_type     = "RSA"
   key_size     = 2048
 
+# Define the key options for the managed services key
   key_opts = [
     "decrypt",
     "encrypt",
@@ -28,6 +30,7 @@ resource "azurerm_key_vault_key" "managed_services" {
   ]
 }
 
+# Define a key in the Azure Key Vault for managed disks
 resource "azurerm_key_vault_key" "managed_disk" {
   depends_on = [azurerm_key_vault_access_policy.terraform]
 
@@ -36,6 +39,7 @@ resource "azurerm_key_vault_key" "managed_disk" {
   key_type     = "RSA"
   key_size     = 2048
 
+# Define the key options for the managed disk key
   key_opts = [
     "decrypt",
     "encrypt",
@@ -46,11 +50,13 @@ resource "azurerm_key_vault_key" "managed_disk" {
   ]
 }
 
+# Define an access policy for the Azure Key Vault
 resource "azurerm_key_vault_access_policy" "terraform" {
   key_vault_id = azurerm_key_vault.this.id
   tenant_id    = azurerm_key_vault.this.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
+# Define the key permissions for the access policy
   key_permissions = [
     "Get",
     "List",
