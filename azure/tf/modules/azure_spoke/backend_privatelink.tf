4c37dcddea70b2c4658e5f47bfb0bf7f4d07e6f7
Tony Bo <tony.bo@databricks.com>
2023-09-12 10:47:49 -0400

    spoke backend_privatelink.tf

--- azure/tf/modules/azure_spoke/backend_privatelink.tf
+++ azure/tf/modules/azure_spoke/backend_privatelink.tf
@@ -1,9 +1,11 @@
+# Define a private endpoint resource for the backend
 resource "azurerm_private_endpoint" "backend" {
   name                = "databricks-pe-backend"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   subnet_id           = azurerm_subnet.privatelink.id
 
+# Configure the private service connection
   private_service_connection {
     name                           = "ple-${var.prefix}-backend"
     private_connection_resource_id = azurerm_databricks_workspace.this.id
@@ -11,6 +13,7 @@ resource "azurerm_private_endpoint" "backend" {
     subresource_names              = ["databricks_ui_api"]
   }
 
+# Configure the private DNS zone group
   private_dns_zone_group {
     name                 = "private-dns-zone-backend"
     private_dns_zone_ids = [azurerm_private_dns_zone.backend.id]
@@ -19,6 +22,7 @@ resource "azurerm_private_endpoint" "backend" {
   tags = var.tags
 }
 
+# Define a private DNS zone resource for the backend
 resource "azurerm_private_dns_zone" "backend" {
   name                = "privatelink.azuredatabricks.net"
   resource_group_name = azurerm_resource_group.this.name
@@ -26,6 +30,7 @@ resource "azurerm_private_dns_zone" "backend" {
   tags = var.tags
 }
 
+# Define a virtual network link for the private DNS zone and the backend virtual network
 resource "azurerm_private_dns_zone_virtual_network_link" "backend" {
   name                  = "databricks-vnetlink-backend"
   resource_group_name   = azurerm_resource_group.this.name
