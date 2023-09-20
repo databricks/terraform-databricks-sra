59b9acc1eff365f27bfd2fdb644bdc3e457628c4
Tony Bo <tony.bo@databricks.com>
2023-09-20 14:22:25 -0400

    comments for spoke peering.tf

--- azure/tf/modules/azure_spoke/peering.tf
+++ azure/tf/modules/azure_spoke/peering.tf
@@ -1,4 +1,4 @@
-
+# Create a virtual network peering from the spoke network to the hub network
 resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   name                      = format("from-%s-to-%s-peer", azurerm_virtual_network.this.name, var.hub_vnet_name)
   resource_group_name       = azurerm_resource_group.this.name
@@ -6,6 +6,7 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   remote_virtual_network_id = var.hub_vnet_id
 }
 
+# Create a virtual network peering from the hub network to the spoke network
 resource "azurerm_virtual_network_peering" "hub_to_spoke" {
   name                      = format("from-%s-to-%s-peer", var.hub_vnet_name, azurerm_virtual_network.this.name)
   resource_group_name       = var.hub_resource_group_name
@@ -13,6 +14,7 @@ resource "azurerm_virtual_network_peering" "hub_to_spoke" {
   remote_virtual_network_id = azurerm_virtual_network.this.id
 }
 
+# Associate the route table with the host subnet
 resource "azurerm_subnet_route_table_association" "host" {
   subnet_id      = azurerm_subnet.host.id
   route_table_id = var.route_table_id
@@ -20,6 +22,7 @@ resource "azurerm_subnet_route_table_association" "host" {
   depends_on = [azurerm_virtual_network_peering.hub_to_spoke, azurerm_virtual_network_peering.spoke_to_hub]
 }
 
+# Associate the route table with the container subnet
 resource "azurerm_subnet_route_table_association" "container" {
   subnet_id      = azurerm_subnet.container.id
   route_table_id = var.route_table_id
@@ -27,11 +30,13 @@ resource "azurerm_subnet_route_table_association" "container" {
   depends_on = [azurerm_virtual_network_peering.hub_to_spoke, azurerm_virtual_network_peering.spoke_to_hub]
 }
 
+# Assign the host subnet CIDR to the IP group
 resource "azurerm_ip_group_cidr" "host" {
   ip_group_id = var.ipgroup_id
   cidr        = local.subnets["host"]
 }
 
+# Assign the container subnet CIDR to the IP group
 resource "azurerm_ip_group_cidr" "container" {
   ip_group_id = var.ipgroup_id
   cidr        = local.subnets["container"]
