0643174242b4605e13d6a7ca031718ababdb5650
jdbraun-db <102988109+jdbraun-db@users.noreply.github.com>
2024-01-31 11:15:17 -0600

    Delete modules directory
    deleting directory

--- modules/workspace_config/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "workspaces_to_configure" {
-  type        = list(string)
-  description = "(Required) List of existing Databricks Workspace IDs to configure."
-}
--- modules/workspace_config/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    databricks = {
-      source  = "databricks/databricks"
-      version = ">=1.6.4"
-    }
-  }
-}
-
