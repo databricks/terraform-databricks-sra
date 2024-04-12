cb21dcb546377cf637e5d8b5812a121d72b30cf2
Scott Stafford <scott.stafford@databricks.com>
2024-04-12 17:36:14 -0400

    [AWS] Custom configuration for workspace is incorrect #46

--- aws/tf/modules/sra/databricks_workspace/workspace_security_modules/admin_configuration/admin_configuration.tf
+++ aws/tf/modules/sra/databricks_workspace/workspace_security_modules/admin_configuration/admin_configuration.tf
@@ -8,9 +8,7 @@ resource "databricks_workspace_conf" "just_config_map" {
     "enable-X-Frame-Options"        = "true",
     "enable-X-Content-Type-Options" = "true",
     "enable-X-XSS-Protection"       = "true",
-    "enableWebTerminal"             = "false",
     "enableDbfsFileBrowser"         = "false",
     "enforceUserIsolation"          = "true",
-    "enableNotebookGitVersioning"   = "true"
   }
 }
