68758acb64c4ba97a9738a2e3b28f682b8e4f4f2
Alex Souza <alexandre.souza@sumersports.com>
2024-05-01 16:39:06 -0300

    fix: change output variables

--- aws/tf/modules/sra/outputs.tf
+++ aws/tf/modules/sra/outputs.tf
@@ -1,3 +1,7 @@
-output "databricks_host" {
+output "workspace_url" {
   value = module.databricks_mws_workspace.workspace_url
+}
+
+output "workspace_id" {
+  value = module.databricks_mws_workspace.workspace_id
 }
---
+++ aws/tf/outputs.tf
@@ -0,0 +1,7 @@
+output "workspace_url" {
+  value = module.SRA.workspace_url
+}
+
+output "workspace_id" {
+  value = module.SRA.workspace_id
+}
