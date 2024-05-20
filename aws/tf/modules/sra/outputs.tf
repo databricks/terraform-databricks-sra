c7bb0912e1bed2059aac691ed935c7fc8e8ca8f2
jorge-sumer <139253279+jorge-sumer@users.noreply.github.com>
2024-05-20 10:50:44 -0400

    Adding service_principal_id to outputs

--- aws/tf/modules/sra/outputs.tf
+++ aws/tf/modules/sra/outputs.tf
@@ -5,3 +5,7 @@ output "workspace_url" {
 output "workspace_id" {
   value = module.databricks_mws_workspace.workspace_id
 }
+
+output "service_principal_id" {
+  value = module.service_principal.service_principal_id
+}
