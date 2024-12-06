68c8c4a41f44f7556f99c0b809d86140fac29e69
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-12-06 12:13:03 +1100

    update description on outputs

--- azure/tf/modules/azure_hub/outputs.tf
+++ azure/tf/modules/azure_hub/outputs.tf
@@ -1,45 +1,56 @@
 output "client_config" {
   value = data.azurerm_client_config.current
+  description = "The client configuration for the current Azure session, including subscription and authentication details."
 }
 
 output "firewall_name" {
   value = azurerm_firewall.this.name
+  description = "The name of the Azure Firewall resource."
 }
 
 output "ipgroup_id" {
   value = azurerm_ip_group.this.id
+  description = "The unique ID of the Azure IP Group."
 }
 
 output "route_table_id" {
   value = azurerm_route_table.this.id
+  description = "The unique ID of the Azure Route Table."
 }
 
 output "key_vault_id" {
   value = length(azurerm_key_vault.this) > 0 ? azurerm_key_vault.this[0].id : null
+  description = "The ID of the Azure Key Vault, if created. Returns null if no Key Vault is created."
 }
 
 output "managed_disk_key_id" {
   value = length(azurerm_key_vault_key.managed_disk) > 0 ? azurerm_key_vault_key.managed_disk[0].id : null
+  description = "The ID of the Key Vault key used for managed disks, if available. Returns null if not created."
 }
 
 output "managed_services_key_id" {
   value = length(azurerm_key_vault_key.managed_services) > 0 ? azurerm_key_vault_key.managed_services[0].id : null
+  description = "The ID of the Key Vault key used for managed services, if available. Returns null if not created."
 }
 
 output "vnet_id" {
   value = azurerm_virtual_network.this.id
+  description = "The unique ID of the Azure Virtual Network."
 }
 
 output "vnet_name" {
   value = azurerm_virtual_network.this.name
+  description = "The name of the Azure Virtual Network."
 }
 
 output "metastore_id" {
   value = databricks_metastore.this.id
+  description = "The unique ID of the Databricks Metastore."
 }
 
 output "resource_group_name" {
   value = azurerm_resource_group.this.name
+  description = "The name of the Azure Resource Group."
 }
 
 output "private_link_info" {
@@ -47,8 +58,10 @@ output "private_link_info" {
     dns_zone_id = azurerm_private_dns_zone.auth_front.id
     subnet_id   = azurerm_subnet.privatelink.id
   }
+  description = "Information related to the Private Link, including DNS Zone ID and Subnet ID for Private Link connectivity."
 }
 
 output "tenant_id" {
   value = local.tenant_id
+  description = "The tenant ID of the Azure subscription, identifying the Azure AD instance."
 }
--- azure/tf/modules/azure_spoke/outputs.tf
+++ azure/tf/modules/azure_spoke/outputs.tf
@@ -1,6 +1,7 @@
 # The value of the "workspace_url" property represents the URL of the Databricks workspace
 output "workspace_url" {
-  value = azurerm_databricks_workspace.this.workspace_url
+  value       = azurerm_databricks_workspace.this.workspace_url
+  description = "The URL of the Databricks workspace, used to access the Databricks environment."
 }
 
 output "ipgroup_cidrs" {
@@ -8,4 +9,5 @@ output "ipgroup_cidrs" {
     ipgroup_host_cidr      = azurerm_ip_group_cidr.host.cidr
     ipgroup_container_cidr = azurerm_ip_group_cidr.container.cidr
   }
+  description = "A map containing the CIDRs for the host and container IP groups, used for network segmentation in Azure."
 }
