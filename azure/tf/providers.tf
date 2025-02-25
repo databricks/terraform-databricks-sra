8c62ceeead7533b31e16938caf673c72cd3986e3
Connor Brown <connor.brown@databricks.com>
2025-02-25 13:01:04 -0500

    chore: Remove unused accounts provider

--- azure/tf/providers.tf
+++ azure/tf/providers.tf
@@ -4,12 +4,6 @@ provider "azurerm" {
 }
 
 provider "databricks" {
-  alias      = "accounts"
-  host       = "https://accounts.azuredatabricks.net"
-  account_id = var.databricks_account_id
-}
-
-provider "databricks" {
   host       = "https://accounts.azuredatabricks.net"
   account_id = var.databricks_account_id
 }
