2615071591a4841814b5a74a4fcc78f024824646
jdbraun <jdbraun@gmail.com>
2025-03-04 20:54:19 -0600

    further formatting and linting

--- aws/tf/modules/sra/databricks_account/audit_log_delivery/variables.tf
+++ aws/tf/modules/sra/databricks_account/audit_log_delivery/variables.tf
@@ -1,7 +1,9 @@
 variable "databricks_account_id" {
+  description = "ID of the Databricks account."
   type = string
 }
 
 variable "resource_prefix" {
+  description = "Prefix for the resource names."
   type = string
 }
