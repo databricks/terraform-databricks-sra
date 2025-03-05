b1143456ebcbce1172d88145ae2cf1ce4a96b849
Connor Brown <connor.brown@databricks.com>
2025-03-05 09:31:03 -0500

    feat(azure): Add support for SAT

--- azure/tf/variables.tf
+++ azure/tf/variables.tf
@@ -56,3 +56,35 @@ variable "subscription_id" {
   type        = string
   description = "(Required) Azure Subscription ID to deploy into"
 }
+
+variable "sat_configuration" {
+  type = object({
+    enabled           = optional(bool, true)
+    spoke             = optional(string, "")
+    schema_name       = optional(string, "sat")
+    catalog_name      = optional(string, "sat")
+    proxies           = optional(map(any), {})
+    run_on_serverless = optional(bool, true)
+  })
+  default = {}
+  validation {
+    condition     = var.sat_configuration.spoke == "" || contains(keys(var.spoke_config), var.sat_configuration.spoke)
+    error_message = "SAT spoke must be a spoke in the spoke_config variable"
+  }
+  description = "(Optional) Configuration for the SAT customization"
+}
+
+variable "sat_service_principal" {
+  type = object({
+    client_id     = optional(string, "")
+    client_secret = optional(string, "")
+    name          = optional(string, "spSAT")
+  })
+  default = {}
+  validation {
+    condition     = var.sat_service_principal.client_id == "" && var.sat_service_principal.client_secret == "" || var.sat_service_principal.client_id != "" && var.sat_service_principal.client_secret != ""
+    error_message = "Both a client_id and client_secret must be provided for SAT if either are provided"
+  }
+  description = "(Optional) Service principal configuration for running SAT. If this is not provided, a service principal will be created. The created service principal name can be configured with the name field in this variable."
+  sensitive   = true
+}
