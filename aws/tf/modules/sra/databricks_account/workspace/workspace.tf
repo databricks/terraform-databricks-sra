d6d3a141e12e9cf1e022cca43caf5023ebb66df1
jdbraun <jdbraun@gmail.com>
2025-03-04 20:37:08 -0600

    format

--- aws/tf/modules/sra/databricks_account/workspace/workspace.tf
+++ aws/tf/modules/sra/databricks_account/workspace/workspace.tf
@@ -86,17 +86,17 @@ resource "databricks_mws_private_access_settings" "pas" {
 resource "databricks_mws_workspaces" "this_with_deployment_name" {
   count = var.deployment_name != null ? 1 : 0
 
-  account_id     = var.databricks_account_id
-  aws_region     = var.region
-  workspace_name = var.resource_prefix
-  deployment_name = var.deployment_name 
-  credentials_id = databricks_mws_credentials.this.credentials_id
-  storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
-  network_id = databricks_mws_networks.this.network_id
-  private_access_settings_id = databricks_mws_private_access_settings.pas.private_access_settings_id
+  account_id                               = var.databricks_account_id
+  aws_region                               = var.region
+  workspace_name                           = var.resource_prefix
+  deployment_name                          = var.deployment_name
+  credentials_id                           = databricks_mws_credentials.this.credentials_id
+  storage_configuration_id                 = databricks_mws_storage_configurations.this.storage_configuration_id
+  network_id                               = databricks_mws_networks.this.network_id
+  private_access_settings_id               = databricks_mws_private_access_settings.pas.private_access_settings_id
   managed_services_customer_managed_key_id = databricks_mws_customer_managed_keys.managed_storage.customer_managed_key_id
-  storage_customer_managed_key_id = databricks_mws_customer_managed_keys.workspace_storage.customer_managed_key_id
-  pricing_tier = "ENTERPRISE"
+  storage_customer_managed_key_id          = databricks_mws_customer_managed_keys.workspace_storage.customer_managed_key_id
+  pricing_tier                             = "ENTERPRISE"
 
   depends_on = [databricks_mws_networks.this]
 }
@@ -105,16 +105,16 @@ resource "databricks_mws_workspaces" "this_with_deployment_name" {
 resource "databricks_mws_workspaces" "this_without_deployment_name" {
   count = var.deployment_name == null ? 1 : 0
 
-  account_id     = var.databricks_account_id
-  aws_region     = var.region
-  workspace_name = var.resource_prefix
-  credentials_id = databricks_mws_credentials.this.credentials_id
-  storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
-  network_id = databricks_mws_networks.this.network_id
-  private_access_settings_id = databricks_mws_private_access_settings.pas.private_access_settings_id
+  account_id                               = var.databricks_account_id
+  aws_region                               = var.region
+  workspace_name                           = var.resource_prefix
+  credentials_id                           = databricks_mws_credentials.this.credentials_id
+  storage_configuration_id                 = databricks_mws_storage_configurations.this.storage_configuration_id
+  network_id                               = databricks_mws_networks.this.network_id
+  private_access_settings_id               = databricks_mws_private_access_settings.pas.private_access_settings_id
   managed_services_customer_managed_key_id = databricks_mws_customer_managed_keys.managed_storage.customer_managed_key_id
-  storage_customer_managed_key_id = databricks_mws_customer_managed_keys.workspace_storage.customer_managed_key_id
-  pricing_tier = "ENTERPRISE"
+  storage_customer_managed_key_id          = databricks_mws_customer_managed_keys.workspace_storage.customer_managed_key_id
+  pricing_tier                             = "ENTERPRISE"
 
   depends_on = [databricks_mws_networks.this]
 }
