04cb5f1e3f07ed65650af6182ec2ed7686a607a0
Antonio Irizarry <antonio.irizarry@databricks.com>
2025-04-18 13:06:55 -0400

    Update jobs.tf

--- aws-gov/tf/modules/sra/security_analysis_tool/common/jobs.tf
+++ aws-gov/tf/modules/sra/security_analysis_tool/common/jobs.tf
@@ -1,35 +1,36 @@
 resource "databricks_job" "initializer" {
-   name = "SAT Initializer Notebook (one-time)"
-   dynamic "job_cluster" {
-     for_each = var.run_on_serverless ? [] : [1]
-     content {
-       job_cluster_key = "job_cluster"
-       new_cluster {
-         data_security_mode = "SINGLE_USER"
-         num_workers        = 5
-         spark_version      = data.databricks_spark_version.latest_lts.id
-         node_type_id       = data.databricks_node_type.smallest.id
-         runtime_engine     = "PHOTON"
-     }
-   }
+  name = "SAT Initializer Notebook (one-time)"
+  dynamic "job_cluster" {
+    for_each = var.run_on_serverless ? [] : [1]
+    content {
+      job_cluster_key = "job_cluster"
+      new_cluster {
+        data_security_mode = "SINGLE_USER"
+        num_workers        = 5
+        spark_version      = data.databricks_spark_version.latest_lts.id
+        node_type_id       = data.databricks_node_type.smallest.id
+        runtime_engine     = "PHOTON"
+      }
+    }
 
-   task {
-     task_key        = "Initializer"
-     job_cluster_key = var.run_on_serverless ? null : "job_cluster"
-     dynamic "library" {
-       for_each = var.run_on_serverless ? [] : [1]
-       content {
-         pypi {
-           package = "dbl-sat-sdk"
-         }
-       }
-     }
-     notebook_task {
-       notebook_path = "${databricks_repo.security_analysis_tool.workspace_path}/notebooks/security_analysis_initializer"
-     }
-   }
+    task {
+      task_key        = "Initializer"
+      job_cluster_key = var.run_on_serverless ? null : "job_cluster"
+      dynamic "library" {
+        for_each = var.run_on_serverless ? [] : [1]
+        content {
+          pypi {
+            package = "dbl-sat-sdk"
+          }
+        }
+      }
+      notebook_task {
+        notebook_path = "${databricks_repo.security_analysis_tool.workspace_path}/notebooks/security_analysis_initializer"
+      }
+    }
 
- }
+  }
+}
 
 resource "databricks_job" "driver" {
   name = "SAT Driver Notebook"
