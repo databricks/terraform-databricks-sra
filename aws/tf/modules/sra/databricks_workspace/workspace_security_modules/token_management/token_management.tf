64418abf003bc6c7b440b67b40f5b40888608d54
Troy Knapp <troy.knapp@sumersports.com>
2025-05-06 15:10:32 -0500

    max life on token

--- aws/tf/modules/sra/databricks_workspace/workspace_security_modules/token_management/token_management.tf
+++ aws/tf/modules/sra/databricks_workspace/workspace_security_modules/token_management/token_management.tf
@@ -3,8 +3,8 @@
 resource "databricks_token" "pat" {
   comment = "Terraform Provisioning"
   // 30 day token
-  lifetime_seconds = 2592000
-  lifecycle {
-    ignore_changes = all
-  }
+  // lifetime_seconds = 2592000
+  // since we only use this for provisioning new workspaces, we don't typically need this,
+  // however, once expired it is reported as drift, so we're going to set it to the max
+  // lifetime
 }
