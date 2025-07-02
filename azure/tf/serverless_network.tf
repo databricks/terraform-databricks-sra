2ae0eb76913b9a42104fe55fd2d1697ead06cd6d
Connor Brown <connor.brown@databricks.com>
2025-07-02 15:48:59 -0400

    tests(azure): Add first precondition check for NCC count

--- azure/tf/serverless_network.tf
+++ azure/tf/serverless_network.tf
@@ -1,15 +1,31 @@
 locals {
   serverless_internet_allowed_domains = [for dest in var.public_repos : dest if !startswith(dest, "*.")]
-  serverless_internet_allowed_destinations = [for dest in local.serverless_internet_allowed_domains : {
-    destination               = trimprefix(dest, "*."),
-    internet_destination_type = "DNS_NAME"
-  }]
+  serverless_internet_allowed_destinations = [
+    for dest in local.serverless_internet_allowed_domains :
+    {
+      destination               = trimprefix(dest, "*."),
+      internet_destination_type = "DNS_NAME"
+    }
+  ]
+
+  # We use this to make sure that if we provision the 10th NCC in a region, that it does not cause subsequent terraform
+  # plans/applies to fail due to the precondition on the NCC resource.
+  ncc_name          = "ncc-${var.location}-${var.hub_resource_suffix}"
+  current_ncc_count = length([for k in data.databricks_mws_network_connectivity_configs.this.names : k if k != local.ncc_name])
+  ncc_region_limit  = 10
 }
 
 # This NCC is shared across all workspaces created by SRA
 resource "databricks_mws_network_connectivity_config" "this" {
-  name   = "ncc-${var.location}-${var.hub_resource_suffix}"
+  name   = local.ncc_name
   region = var.location
+
+  lifecycle {
+    precondition {
+      condition     = local.current_ncc_count < local.ncc_region_limit
+      error_message = "There are already ${local.ncc_region_limit} NCCs in ${var.location}!"
+    }
+  }
 }
 
 resource "databricks_account_network_policy" "restrictive_network_policy" {
