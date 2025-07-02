350938e8713c97756b5b389c3b6f499c62f7d11b
Connor Brown <connor.brown@databricks.com>
2025-07-02 11:54:10 -0400

    fix(azure): Force destroy storage credential and external location when force_destroy is true for SAT catalog

--- azure/tf/serverless_network.tf
+++ azure/tf/serverless_network.tf
@@ -1,6 +1,28 @@
+locals {
+  serverless_internet_allowed_domains = [for dest in var.public_repos : dest if !startswith(dest, "*.")]
+  serverless_internet_allowed_destinations = [for dest in local.serverless_internet_allowed_domains : {
+    destination               = trimprefix(dest, "*."),
+    internet_destination_type = "DNS_NAME"
+  }]
+}
+
 # This NCC is shared across all workspaces created by SRA
 resource "databricks_mws_network_connectivity_config" "this" {
   name   = "ncc-${var.location}-${var.hub_resource_suffix}"
   region = var.location
 }
 
+resource "databricks_account_network_policy" "restrictive_network_policy" {
+  account_id        = var.databricks_account_id
+  network_policy_id = "np-${var.hub_resource_suffix}-restrictive"
+
+  egress = {
+    network_access = {
+      restriction_mode              = "RESTRICTED_ACCESS"
+      allowed_internet_destinations = local.serverless_internet_allowed_destinations
+      policy_enforcement = {
+        enforcement_mode = "ENFORCED"
+      }
+    }
+  }
+}
