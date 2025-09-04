58018af9c4d0a6a3acab6d2cf32e9e081f927a52
Connor Brown <connor.brown@databricks.com>
2025-09-04 10:59:08 -0400

    docs(azure): Remove resource ID in comment

--- azure/tf/modules/catalog/main.tf
+++ azure/tf/modules/catalog/main.tf
@@ -21,7 +21,6 @@ resource "databricks_storage_credential" "unity_catalog" {
 
   provider = databricks.workspace
 }
-# https://adb-745607464060552.12.azuredatabricks.net/aad/auth?has=&Workspace=/subscriptions/edd4cc45-85c7-4aec-8bf5-648062d519bf/resourceGroups/rg-cbtsthub/providers/Microsoft.Databricks/workspaces/WEB_AUTH_DO_NOT_DELETE_WESTUS3&WorkspaceResourceGroupUri=/subscriptions/edd4cc45-85c7-4aec-8bf5-648062d519bf/resourceGroups/rg-cbtsthub-nv63adbmanaged&l=en
 resource "databricks_external_location" "external_location" {
   credential_name = databricks_storage_credential.unity_catalog.name
   name            = azurerm_storage_account.unity_catalog.name
