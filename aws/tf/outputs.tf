ae3b10f2f3e78bbf2c509dbe3edc58e50eb50752
Connor Brown <connor.brown@databricks.com>
2025-09-12 14:09:09 -0400

    tests(aws): Implement AWS integration test

--- aws/tf/outputs.tf
+++ aws/tf/outputs.tf
@@ -1,3 +1,8 @@
 output "workspace_host" {
   value = module.databricks_mws_workspace.workspace_url
 }
+
+output "catalog_name" {
+  description = "Name of the catalog created for the workspace"
+  value       = module.unity_catalog_catalog_creation.catalog_name
+}
--- common/tests/classic_cluster/outputs.tf
+++ common/tests/classic_cluster/outputs.tf
@@ -2,3 +2,13 @@ output "cluster_id" {
   description = "Cluster ID"
   value       = databricks_cluster.test_cluster.cluster_id
 }
+
+output "node_type_id" {
+  description = "Type of node used"
+  value       = data.databricks_node_type.smallest.id
+}
+
+output "spark_version" {
+  description = "Spark version used"
+  value       = data.databricks_spark_version.latest.id
+}
