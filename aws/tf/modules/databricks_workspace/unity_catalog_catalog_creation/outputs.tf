ef790d63ac970daef148a94a63f5c9773efed231
Connor Brown <connor.brown@databricks.com>
2025-09-12 14:09:09 -0400

    tests(aws): Implement AWS integration test

--- aws/tf/modules/databricks_workspace/unity_catalog_catalog_creation/outputs.tf
+++ aws/tf/modules/databricks_workspace/unity_catalog_catalog_creation/outputs.tf
@@ -2,3 +2,8 @@ output "catalog_bucket_name" {
   description = "Catalog bucket name."
   value       = aws_s3_bucket.unity_catalog_bucket.bucket
 }
+
+output "catalog_name" {
+  description = "Name of the catalog created"
+  value       = databricks_catalog.workspace_catalog.name
+}
