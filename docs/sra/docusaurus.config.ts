c9d5252a601854d2622f6e8c7e5e9c36393adc07
Shreel Shah <shreelshah12@gmail.com>
2025-10-15 10:11:26 -0400

    base url and azure arch update

--- docs/sra/docusaurus.config.ts
+++ docs/sra/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://databricks.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/security-reference-architecture/',
+  baseUrl: '/terraform-databricks-sra/',
   trailingSlash: true,
 
   // GitHub pages deployment config.
