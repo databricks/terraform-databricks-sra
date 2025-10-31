261614eee59d5e5388e7bc653aea8d1aedcd93aa
Connor Brown <connor.brown@databricks.com>
2025-10-31 12:47:37 -0400

    feat(azure): Depend on vnet rg name

--- azure/tf/modules/workspace/network.tf
+++ azure/tf/modules/workspace/network.tf
@@ -5,18 +5,18 @@ data "azurerm_virtual_network" "this" {
 
 data "azurerm_subnet" "private" {
   name                 = var.network_configuration.private_subnet_name
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = data.azurerm_virtual_network.this.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.this.name
 }
 
 data "azurerm_subnet" "public" {
   name                 = var.network_configuration.public_subnet_name
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = data.azurerm_virtual_network.this.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.this.name
 }
 
 data "azurerm_subnet" "private_endpoint" {
   name                 = var.network_configuration.private_endpoint_subnet_name
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = data.azurerm_virtual_network.this.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.this.name
 }
