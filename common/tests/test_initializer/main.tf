555b56a7460d3cfca71944d1d1dc97c9c6e71b4a
Connor Brown <connor.brown@databricks.com>
2025-11-04 11:08:18 -0500

    tests: Support using state workspaces while testing

--- common/tests/test_initializer/main.tf
+++ common/tests/test_initializer/main.tf
@@ -1,7 +1,14 @@
+# This is used to fetch the current state workspace. The terraform test framework does not support using the typical
+# terraform.workspace to fetch this information.
+data "external" "workspace" {
+  program     = ["sh", "-c", "echo '{\"name\":\"'$(terraform workspace show)'\"}'"]
+  working_dir = path.cwd
+}
+
 data "terraform_remote_state" "state" {
   backend = "local"
 
   config = {
-    path = "${path.cwd}/terraform.tfstate"
+    path = data.external.workspace.result.name == "default" ? "${path.cwd}/terraform.tfstate" : "${path.cwd}/terraform.tfstate.d/${data.external.workspace.result.name}/terraform.tfstate"
   }
 }
