d6459137e9b14fbae1d68620fb83e7506e8f700f
Connor Brown <connor.brown@databricks.com>
2026-06-23 15:11:24 -0400

    fix(azure): Remove unused uc-owners group

--- azure/tf/modules/hub/unitycatalog.tf
+++ azure/tf/modules/hub/unitycatalog.tf
@@ -2,14 +2,7 @@
 resource "databricks_metastore" "this" {
   count = var.is_unity_catalog_enabled ? 1 : 0
 
-  name = "uc-metastore-${var.resource_suffix}"
-  # owner         = "uc admins"
+  name          = "uc-metastore-${var.resource_suffix}"
   region        = var.location
   force_destroy = var.force_destroy
-}
-
-resource "databricks_group" "this" {
-  count = var.is_unity_catalog_enabled ? 1 : 0
-
-  display_name = "${var.resource_suffix}-uc-owners"
 }
